b34829559835ec6d5f5841f5b89fa21f561d217a
acse-moh2217 <moh2217@ic.ac.uk>
2020-10-21 11:42:02 +0100

    hMMMMMmmmMM

--- simple_functions/functions1.py
+++ simple_functions/functions1.py
@@ -13,3 +13,15 @@ def my_sum(iterable):
 @lru_cache(maxsize=None)  # Note: -> @cache in python >= 3.9
 def factorial(n):
     return n * factorial(n-1) if n else 1
+
+def sin(x,n):
+    s = 0
+    for i in range(n):
+        sign = (-1)**i
+        pi=22/7
+        y=x*(pi/180)
+        s = s + ((y**(2.0*i+1))/factorial(2*i+1))*sign
+    return s
+x=int(input("x in degrees: "))
+n=int(input("number of terms "+"'n'" ": "))
+print(round(sin(x,n),2))
